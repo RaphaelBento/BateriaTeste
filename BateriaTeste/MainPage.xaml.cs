d3a581b8fe14570f21ae1b5dfc35dc3029225831
Raphael Bento <rafhaesilvalbento@gmail.com>
2021-05-03 15:57:18 -0300

    Adicionar arquivos de projeto.

---
+++ BateriaTeste/MainPage.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Power;
+using Windows.UI.Core;
+
+namespace BateriaTeste
+{
+    public sealed partial class MainPage : Page
+    {
+        bool reportRequested = false;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Battery.AggregateBattery.ReportUpdated += AggregateBattery_ReportUpdated;
+        }
+
+
+        private void GetBatteryReport(object sender, RoutedEventArgs e)
+        {
+            // Clear UI
+            BatteryReportPanel.Children.Clear();
+
+
+            if (BotaoTeste.IsChecked == true)
+            {
+                // Request aggregate battery report
+                RequestAggregateBatteryReport();
+            }
+            else
+            {
+                // Request individual battery report
+                RequestIndividualBatteryReports();
+            }
+
+            // Note request
+            reportRequested = true;
+        }
+
+        private void RequestAggregateBatteryReport()
+        {
+            // Create aggregate battery object
+            var aggBattery = Battery.AggregateBattery;
+
+            // Get report
+            var report = aggBattery.GetReport();
+
+            // Update UI
+            AddReportUI(BatteryReportPanel, report, aggBattery.DeviceId);
+        }
+
+        async private void RequestIndividualBatteryReports()
+        {
+            // Find batteries 
+            var deviceInfo = await DeviceInformation.FindAllAsync(Battery.GetDeviceSelector());
+            foreach (DeviceInformation device in deviceInfo)
+            {
+                try
+                {
+                    // Create battery object
+                    var battery = await Battery.FromIdAsync(device.Id);
+
+                    // Get report
+                    var report = battery.GetReport();
+
+                    // Update UI
+                    AddReportUI(BatteryReportPanel, report, battery.DeviceId);
+                }
+                catch { /* Add error handling, as applicable */ }
+            }
+        }
+
+
+        private void AddReportUI(StackPanel sp, BatteryReport report, string DeviceID)
+        {
+            // Create battery report UI
+            TextBlock txt1 = new TextBlock { Text = "Device ID: " + DeviceID };
+            txt1.FontSize = 15;
+            txt1.Margin = new Thickness(0, 15, 0, 0);
+            txt1.TextWrapping = TextWrapping.WrapWholeWords;
+
+            TextBlock txt2 = new TextBlock { Text = " status da bateria: " + report.Status.ToString() };
+            txt2.FontStyle = Windows.UI.Text.FontStyle.Italic;
+            txt2.Margin = new Thickness(0, 0, 0, 15);
+
+            TextBlock txt3 = new TextBlock { Text = "taxa de carga(miliWhats): " + report.ChargeRateInMilliwatts.ToString() };
+            TextBlock txt4 = new TextBlock { Text = "Capacidade da bateria em (mWh): " + report.DesignCapacityInMilliwattHours.ToString() };
+            TextBlock txt5 = new TextBlock { Text = "Capacidade da bateria totalmente carregada (mWh): " + report.FullChargeCapacityInMilliwattHours.ToString() };
+            TextBlock txt6 = new TextBlock { Text = "Capacidade restante da bateria (mWh): " + report.RemainingCapacityInMilliwattHours.ToString() };
+
+            // Create energy capacity progress bar & labels
+            TextBlock pbLabel = new TextBlock { Text = "Porcentagem restante da bateria" };
+            pbLabel.Margin = new Thickness(0, 10, 0, 5);
+            pbLabel.FontFamily = new FontFamily("Segoe UI");
+            pbLabel.FontSize = 11;
+
+            ProgressBar pb = new ProgressBar();
+            pb.Margin = new Thickness(0, 5, 0, 0);
+            pb.Width = 200;
+            pb.Height = 10;
+            pb.IsIndeterminate = false;
+            pb.HorizontalAlignment = HorizontalAlignment.Left;
+
+            TextBlock pbPercent = new TextBlock();
+            pbPercent.Margin = new Thickness(0, 5, 0, 10);
+            pbPercent.FontFamily = new FontFamily("Segoe UI");
+            pbLabel.FontSize = 11;
+
+            // Disable progress bar if values are null
+            if ((report.FullChargeCapacityInMilliwattHours == null) ||
+                (report.RemainingCapacityInMilliwattHours == null))
+            {
+                pb.IsEnabled = false;
+                pbPercent.Text = "N/A";
+            }
+            else
+            {
+                pb.IsEnabled = true;
+                pb.Maximum = Convert.ToDouble(report.FullChargeCapacityInMilliwattHours);
+                pb.Value = Convert.ToDouble(report.RemainingCapacityInMilliwattHours);
+                pbPercent.Text = ((pb.Value / pb.Maximum) * 100).ToString("F2") + "%";
+            }
+
+            // Add controls to stackpanel
+            sp.Children.Add(txt1);
+            sp.Children.Add(txt2);
+            sp.Children.Add(txt3);
+            sp.Children.Add(txt4);
+            sp.Children.Add(txt5);
+            sp.Children.Add(txt6);
+            sp.Children.Add(pbLabel);
+            sp.Children.Add(pb);
+            sp.Children.Add(pbPercent);
+        }
+
+        async private void AggregateBattery_ReportUpdated(Battery sender, object args)
+        {
+            if (reportRequested)
+            {
+
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    // Clear UI
+                    BatteryReportPanel.Children.Clear();
+
+
+                    if (BotaoTeste.IsChecked == true)
+                    {
+                        // Request aggregate battery report
+                        RequestAggregateBatteryReport();
+                    }
+                    else
+                    {
+                        // Request individual battery report
+                        RequestIndividualBatteryReports();
+                    }
+                });
+            }
+        }
+    }
+}
